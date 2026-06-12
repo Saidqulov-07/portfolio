ac773d3d9c741eacff8836a0b69e409a9579d583
Saidqulov Samandar <samandarsaidqulov977@gmail.com>
2026-06-12 14:38:14 +0500

    Ilovalar uchun to'liq yo'l (path) qo'shildi

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio_project.aloqa',  # Sizning ilovangiz nomi
+    'aloqa',  # Sizning ilovangiz nomi
     'portfel', # Sizning ikkinchi ilovangiz nomi
 ]
 
