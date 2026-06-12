07cf16062b89c32370387bb85e5371aeca442f7e
Saidqulov Samandar <samandarsaidqulov977@gmail.com>
2026-06-12 13:58:57 +0500

    Fix NameError in wsgi.py

--- portfolio_project/wsgi.py
+++ portfolio_project/wsgi.py
@@ -1,5 +1,7 @@
-# Eski qator:
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.sozlamalar')
+import os
+from django.core.wsgi import get_wsgi_application
 
-# Yangi qator (agar fayl nomi settings.py bo'lsa):
+# 'portfolio_project.settings' bu sizning settings faylingizning yo'li
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
+
+application = get_wsgi_application()
